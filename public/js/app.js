63620f729f4ddf936a196288aff8bf42cf78974b
Kyle <kyle@Kyles-MacBook-Pro.local>
2017-10-15 23:09:13 -0600

    ADD google autocomplete feature

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,23 @@
   function WeatherController($scope, $http) {
     $scope.createWeather = createWeather;
 
+    var input = document.getElementById('inputCity');
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+    var options = {
+      types: ['(cities)']
+    };
+
     // Things to load first
     function init() {
       $scope.city = {
-        name: 'Denver'
+        name: 'Denver, CO, United States'
       }
+
       createWeather($scope.city);
     }
-    init(); // call init
 
+    // Google autocomplete
+    google.maps.event.addDomListener(window, 'load', init);
     /*
     function clearFields() {
       $scope.city.name = "";
@@ -22,18 +30,25 @@
 
     // Create a post
     function createWeather(city) {
-      console.log(city);
+
+      $scope.currentCity = input.value;
+      console.log($scope.currentCity);
+      
+      if ($scope.currentCity) {
+        city.name = $scope.currentCity;
+      } else {
+        $scope.currentCity = 'Denver, CO, United States';
+      }
 
       $http
         .post("/api/weather", city) // not saying which one so must be all
         .then(function(res) {
           $scope.weather = res.data;
-            console.log(res.data);
-            console.log(res.data.currently.summary);
-
-            // Week
-            console.log(res.data.daily.data);
-            //clearFields();
+          // Current day
+          console.log(res.data);
+          // Week
+          console.log(res.data.daily.data);
+          //clearFields();
         });
     }
   }
